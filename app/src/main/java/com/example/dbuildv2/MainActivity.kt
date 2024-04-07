17801931eb5b098c0b16d0dbc42b86461821c5db
knyaz <vnguitar@mail.ru>
2024-04-07 20:11:01 +0300

    Added registration

--- app/src/main/java/com/example/dbuildv2/MainActivity.kt
+++ app/src/main/java/com/example/dbuildv2/MainActivity.kt
@@ -1,11 +1,65 @@
 package com.example.dbuildv2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.registration_acticity)
+
+        val lastNameInput: EditText = findViewById(R.id.last_name)
+        val firstNameInput: EditText = findViewById(R.id.first_name)
+        val phoneNumberInput: EditText = findViewById(R.id.phone_number)
+        val passwordInput: EditText = findViewById(R.id.pwd)
+        val passwordRepeatInput: EditText = findViewById(R.id.pwd_repeat)
+        val acceptPersonalData: CheckBox = findViewById(R.id.personal_data)
+        val acceptTermsOfUse: CheckBox = findViewById(R.id.terms_of_use)
+        val regButton: Button = findViewById(R.id.reg_btn)
+        val toLoginButton: Button = findViewById(R.id.to_log_btn)
+
+        regButton.setOnClickListener {
+            if (!acceptPersonalData.isChecked || !acceptTermsOfUse.isChecked) {
+                Toast.makeText(this,"Регистрация невозможна без принятия условий соглашения.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val lastName = lastNameInput.text.toString().trim()
+            val firstName = firstNameInput.text.toString().trim()
+            val phoneNumber = phoneNumberInput.text.toString().trim()
+            val password = passwordInput.text.toString().trim()
+            val passwordRepeat = passwordRepeatInput.text.toString().trim()
+
+            if (lastName == "" || firstName == "") {
+                Toast.makeText(this,"Введите фамилию и имя.", Toast.LENGTH_SHORT).show()
+            } else if (password.length < 8) {
+                Toast.makeText(this,"Минимальная длина пароля - 8 символов.", Toast.LENGTH_SHORT).show()
+            } else if (password != passwordRepeat) {
+                Toast.makeText(this,"Введенные пароли не совпадают.", Toast.LENGTH_SHORT).show()
+            } else if (phoneNumber.length != 12 || phoneNumber[0] != '+' || phoneNumber[1] != '7') {
+                Toast.makeText(this,"Введите корректный номер телефона через +7.", Toast.LENGTH_SHORT).show()
+            } else {
+                val user = User(phoneNumber, lastName, firstName, password)
+
+                val db = DBHelper(this, null)
+                db.addUser(user)
+
+                Toast.makeText(this,"Регистрация прошла успешно.", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, authorisation_activity::class.java)
+                startActivity(intent)
+            }
+        }
+
+        toLoginButton.setOnClickListener {
+            val intent = Intent(this, authorisation_activity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
