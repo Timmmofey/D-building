95b142b3b68487785166fc2df6ad5b2d4724cdf9
knyaz7 <vn.guitar@mail.ru>
2024-04-12 13:48:19 +0300

    Dependencies change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,5 +48,7 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.viewpager2:viewpager2:1.0.0")
     implementation("com.squareup.picasso:picasso:2.8")
-
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
